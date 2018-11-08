81a8985a012259c8c8b74880c475912546667298
DavAna <david@anaton.fr>
2018-11-08 12:58:41 +0200

    Added level5

---
+++ level_5/expanding_nebula/test.py
@@ -0,0 +1,16 @@
+import solution, sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '../..'))
+import create_check_function as create
+
+test_cases = [
+	[[True, False, True], [False, True, False], [True, False, True]],
+	[[True, False, True, False, False, True, True, True], [True, False, True, False, False, False, True, False], [True, True, True, False, False, False, True, False], [True, False, True, False, False, False, True, False], [True, False, True, False, False, True, True, True]],
+	[[True, True, False, True, False, True, False, True, True, False], [True, True, False, False, False, False, True, True, True, False], [True, True, False, False, False, False, False, False, False, True], [False, True, False, False, False, False, True, True, False, False]],
+]
+expected = [
+	4,
+	254,
+	11567,
+]
+
+create.check_function(test_cases, expected, solution.answer)(0)
